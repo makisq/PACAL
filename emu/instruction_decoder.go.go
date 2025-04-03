17a8b09f94d7e1872a1016108b3e9805a9ce2c5a
makisq <33791824+makisq@users.noreply.github.com>
2025-04-04 02:48:26 +0300

    Update instruction_decoder.go.go

--- emu/instruction_decoder.go.go
+++ emu/instruction_decoder.go.go
@@ -19,26 +19,39 @@ func DecodeInstruction(instr [4]bool) Instruction {
 
 	decoded := Instruction{OpCode: op}
 
-	if op == OpHalt {
+	switch op {
+	case OpHalt:
+		return decoded
+
+	case OpCall:
+		decoded.Reg1 = boolToInt(instr[2])
+		decoded.Reg2 = -1
+		return decoded
+
+	case OpMov:
+		if instr[0] {
+			decoded.Imm = [4]bool{instr[1], instr[2], instr[3], false}
+			decoded.Reg2 = -1
+		} else {
+			decoded.Reg1 = boolToInt(instr[1])
+			decoded.Reg2 = boolToInt(instr[2])
+		}
+		return decoded
+
+	case OpLoad, OpStore:
+		decoded.Reg1 = boolToInt(instr[1])
+		decoded.Reg2 = boolToInt(instr[2])
+		decoded.Address = [4]bool{false, instr[1], instr[2], instr[3]}
+		return decoded
+
+	case OpJmp, OpJz, OpJnz, OpJc:
+		decoded.Reg1 = boolToInt(instr[1])
+		decoded.Address = [4]bool{false, instr[1], instr[2], instr[3]}
+		return decoded
+
+	default:
+		decoded.Reg1 = boolToInt(instr[1])
+		decoded.Reg2 = boolToInt(instr[2])
 		return decoded
 	}
-
-	if op >= 0b1000 && op != OpHalt {
-		decoded.Reg1 = boolToInt(instr[2])
-		decoded.Reg2 = boolToInt(instr[3])
-	} else {
-		decoded.Reg1 = boolToInt(instr[1])
-		decoded.Reg2 = boolToInt(instr[0])
-	}
-
-	if op == OpMov && instr[0] {
-		decoded.Imm = [4]bool{instr[1], instr[2], instr[3], false}
-		decoded.Reg2 = -1
-	}
-
-	if op == OpLoad || op == OpStore || (op >= OpJmp && op <= OpJc) {
-		decoded.Address = [4]bool{false, instr[1], instr[2], instr[3]}
-	}
-
-	return decoded
 }
